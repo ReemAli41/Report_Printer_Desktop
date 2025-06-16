371e8ee3799297f95fefbac84c6e91a66adbd324
ReemAli41 <reem.ali.abdelrahman@gmail.com>
2025-06-16 11:29:01 +0300

    Add project files.

---
+++ Report_Printer_Desktop/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Report_Printer_Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
